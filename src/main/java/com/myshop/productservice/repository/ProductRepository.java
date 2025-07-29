45598aad0668b1dfb71c6249743f5c585af6898d
CherteelA <asitnikov770@gmail.com>
2025-07-29 13:01:05 +0300

    add ProductRepository

---
+++ src/main/java/com/myshop/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.myshop.productservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    
+}
